1c24e503c2c4fc397243a8df4648bbbd774edaf4
mobile-2025 <leandrocesardacruz.mobile.2025@gmail.com>
2025-03-11 12:25:07 -0300

    Comando de print e sequencia de scape

---
+++ src/ClassePrincipal.java
@@ -0,0 +1,28 @@
+
+public class ClassePrincipal {
+
+	public static void main(String[] args) {
+		
+		// Printar mensagem pulando linha 
+		System.out.println("Pulando linha"); //sysout + space
+		
+		// Printar mensagem pulando linha 
+		System.out.printf("Sem pular linha"); //sysout + space
+		
+		System.out.println("Pulando linha"); //sysout + space
+		
+		// Sequências de scape
+		// Usar aspas duplas
+		System.out.println("Meu nome é \"Fulano\"");
+		// Usar aspas simples
+		System.out.println("Meu nome é \'Fulano\'");
+		// Pular linha, quebrar linha
+		System.out.println("Meu nome é \n Fulano");
+
+	}
+
+}
+
+
+
+
